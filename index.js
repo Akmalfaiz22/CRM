97502f1f5d1eb226363e1e86be1b1d6bddbb7205
akmalfaiz22 <akmalfaiz10001@gmail.com>
2026-08-12 13:56:45 +0530

    feat: add tablet responsiveness and JS functionality

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const revenueChart = document.getElementById('revenueChart');
 const projectStatus = document.getElementById('projectStatus');
+const inboxSide = document.getElementById('inboxSide');
+const clientSide = document.getElementById('clientSide');
+const projectSide = document.getElementById('projectSide');
+const taskSide = document.getElementById('taskSide');
+const kanbanSide = document.getElementById('kanbanSide');
+const teamSide = document.getElementById('teamSide');
+const calenderSide = document.getElementById('calenderSide');
+const paymentSide = document.getElementById('paymentSide');
+const invoiceSide = document.getElementById('invoiceSide');
+const expensesSide = document.getElementById('expensesSide');
+const analyticsSide = document.getElementById('analyticsSide');
+const revenueSide = document.getElementById('revenueSide');
+const performanceSide = document.getElementById('performanceSide');
+const documentSide = document.getElementById('documentSide');
+const mediaSide = document.getElementById('mediaSide');
+const filesSide = document.getElementById('filesSide');
+const messageSide = document.getElementById('messageSide');
+const helpSide = document.getElementById('helpSide');
+const settingSide = document.getElementById('settingSide');
 
 new Chart(revenueChart, {
  type: 'line',
@@ -71,3 +90,47 @@ new Chart(projectStatus, {
   },
     
 });
+
+inboxSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/inbox.html"
+})
+
+clientSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/client.html"
+})
+
+projectSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/project.html"
+})
+
+taskSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/task.html"
+})
+
+kanbanSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/kanban.html"
+})
+
+teamSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/team.html"
+})
+
+calenderSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/calendar.html"
+})
+
+paymentSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/payment.html"
+})
+
+invoiceSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/invoice.html"
+})
+
+expensesSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/expenses.html"
+})
+
+analyticsSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/analytics.html"
+})
