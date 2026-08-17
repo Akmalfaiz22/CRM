0fdd5062fb789cd0d3c15888f9eed7315d02746b
akmalfaiz22 <akmalfaiz10001@gmail.com>
2026-08-17 11:13:12 +0530

    Refactor navigation in task and team pages; add event listeners for sidebar items and update styles in CSS for improved layout and responsiveness

--- kanban.js
+++ kanban.js
@@ -1,8 +1,27 @@
-const todoBtn = document.getElementById('todoBtn')
-const progressBtn = document.getElementById('progressBtn')
-const reviewBtn = document.getElementById('reviewBtn')
-const doneBtn = document.getElementById('doneBtn')
-const blockBtn = document.getElementById('blockBtn')
+const todoBtn = document.getElementById('todoBtn');
+const progressBtn = document.getElementById('progressBtn');
+const reviewBtn = document.getElementById('reviewBtn');
+const doneBtn = document.getElementById('doneBtn');
+const blockBtn = document.getElementById('blockBtn');
+const indexSide = document.getElementById('indexSide');
+const inboxSide = document.getElementById('inboxSide');
+const clientSide = document.getElementById('clientSide');
+const projectSide = document.getElementById('projectSide');
+const taskSide = document.getElementById('taskSide');
+const teamSide = document.getElementById('teamSide');
+const calenderSide = document.getElementById('calenderSide');
+const paymentSide = document.getElementById('paymentSide');
+const invoiceSide = document.getElementById('invoiceSide');
+const expensesSide = document.getElementById('expensesSide');
+const analyticsSide = document.getElementById('analyticsSide');
+const revenueSide = document.getElementById('revenueSide');
+const performanceSide = document.getElementById('performanceSide');
+const documentSide = document.getElementById('documentSide');
+const mediaSide = document.getElementById('mediaSide');
+const filesSide = document.getElementById('filesSide');
+const messageSide = document.getElementById('messageSide');
+const helpSide = document.getElementById('helpSide');
+const settingSide = document.getElementById('settingSide');
 
 const sections = ["todo", "progress", "review", "done", "block"];
 
@@ -33,3 +52,47 @@ doneBtn.addEventListener("click", () => {
 blockBtn.addEventListener("click", () => {
     showSection("block")
 });
+
+indexSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/index.html"
+})
+
+inboxSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/inbox.html"
+})
+
+clientSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/client.html"
+})
+
+projectSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/project.html"
+})
+
+taskSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/task.html"
+})
+
+teamSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/team.html"
+})
+
+calenderSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/calendar.html"
+})
+
+paymentSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/payment.html"
+})
+
+invoiceSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/invoice.html"
+})
+
+expensesSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/expenses.html"
+})
+
+analyticsSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/analytics.html"
+})
---
+++ project.js
@@ -0,0 +1,63 @@
+const indexSide = document.getElementById('indexSide');
+const inboxSide = document.getElementById('inboxSide');
+const clientSide = document.getElementById('clientSide');
+const taskSide = document.getElementById('taskSide');
+const kanbanSide = document.getElementById('kanbanSide');
+const teamSide = document.getElementById('teamSide');
+const calenderSide = document.getElementById('calenderSide');
+const paymentSide = document.getElementById('paymentSide');
+const invoiceSide = document.getElementById('invoiceSide');
+const expensesSide = document.getElementById('expensesSide');
+const analyticsSide = document.getElementById('analyticsSide');
+const revenueSide = document.getElementById('revenueSide');
+const performanceSide = document.getElementById('performanceSide');
+const documentSide = document.getElementById('documentSide');
+const mediaSide = document.getElementById('mediaSide');
+const filesSide = document.getElementById('filesSide');
+const messageSide = document.getElementById('messageSide');
+const helpSide = document.getElementById('helpSide');
+const settingSide = document.getElementById('settingSide');
+
+indexSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/index.html"
+})
+
+inboxSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/inbox.html"
+})
+
+clientSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/client.html"
+})
+
+taskSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/task.html"
+})
+
+kanbanSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/kanban.html"
+})
+
+teamSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/team.html"
+})
+
+calenderSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/calendar.html"
+})
+
+paymentSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/payment.html"
+})
+
+invoiceSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/invoice.html"
+})
+
+expensesSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/expenses.html"
+})
+
+analyticsSide.addEventListener("click", () => {
+  location.href = "http://localhost:3000/analytics.html"
+})
