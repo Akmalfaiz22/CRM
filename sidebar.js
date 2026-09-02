af81b56d1ada618aeb8f3d1e9182690c3fd6ee3e
akmalfaiz22 <akmalfaiz10001@gmail.com>
2026-09-02 09:11:34 +0530

    feat: refactor sidebar navigation for improved maintainability and update package scripts

--- sidebar.js
+++ sidebar.js
@@ -1,105 +1,33 @@
-const indexSide = document.getElementById('indexSide');
-const inboxSide = document.getElementById('inboxSide');
-const clientSide = document.getElementById('clientSide');
-const projectSide = document.getElementById('projectSide');
-const taskSide = document.getElementById('taskSide');
-const kanbanSide = document.getElementById('kanbanSide');
-const teamSide = document.getElementById('teamSide');
-const calenderSide = document.getElementById('calenderSide');
-const paymentSide = document.getElementById('paymentSide');
-const invoiceSide = document.getElementById('invoiceSide');
-const expensesSide = document.getElementById('expensesSide');
-const analyticsSide = document.getElementById('analyticsSide');
-const revenueSide = document.getElementById('revenueSide');
-const performanceSide = document.getElementById('performanceSide');
-const documentSide = document.getElementById('documentSide');
-const mediaSide = document.getElementById('mediaSide');
-const filesSide = document.getElementById('filesSide');
-const messageSide = document.getElementById('messageSide');
-const helpSide = document.getElementById('helpSide');
-const settingSide = document.getElementById('settingSide');
-const notificationNav = document.getElementById('notificationNav');
+const pages = {
+    indexSide: "index.html",
+    inboxSide: "inbox.html",
+    clientSide: "client.html",
+    projectSide: "project.html",
+    taskSide: "task.html",
+    kanbanSide: "kanban.html",
+    teamSide: "team.html",
+    calenderSide: "calendar.html",
+    paymentSide: "payment.html",
+    invoiceSide: "invoice.html",
+    expensesSide: "expenses.html",
+    analyticsSide: "analytics.html",
+    revenueSide: "revenue.html",
+    performanceSide: "performance.html",
+    documentSide: "document.html",
+    mediaSide: "media.html",
+    filesSide: "file.html",
+    messageSide: "message.html",
+    helpSide: "help.html",
+    settingSide: "setting.html",
+    notificationNav: "notification.html"
+};
 
-indexSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/index.html"
-})
+Object.entries(pages).forEach(([id, page]) => {
+    const element = document.getElementById(id);
 
-inboxSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/inbox.html"
-})
-
-clientSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/client.html"
-})
-
-projectSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/project.html"
-})
-
-taskSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/task.html"
-})
-
-kanbanSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/kanban.html"
-})
-
-teamSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/team.html"
-})
-
-calenderSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/calendar.html"
-})
-
-paymentSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/payment.html"
-})
-
-invoiceSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/invoice.html"
-})
-
-expensesSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/expenses.html"
-})
-
-analyticsSide.addEventListener("click", () => {
-  location.href = "http://localhost:3000/analytics.html"
-})
-
-revenueSide.addEventListener("click", () => {
-    location.href = "http://localhost:3000/revenue.html"
-})
-
-performanceSide.addEventListener("click", () => {
-    location.href = "http://localhost:3000/performance.html"
-})
-
-messageSide.addEventListener("click", () => {
-    location.href = "http://localhost:3000/message.html"
-})
-
-notificationNav.addEventListener("click", () => {
-    location.href = "http://localhost:3000/notification.html"
-})
-
-helpSide.addEventListener("click", () => {
-    location.href = "http://localhost:3000/help.html"
-})
-
-settingSide.addEventListener("click", () => {
-    location.href = "http://localhost:3000/setting.html"
-})
-
-documentSide.addEventListener("click", () => {
-    location.href = "http://localhost:3000/document.html"
-})
-
-mediaSide.addEventListener("click", () => {
-    location.href = "http://localhost:3000/media.html"
-})
-
-filesSide.addEventListener("click", () => {
-    location.href = "http://localhost:3000/file.html"
-})
+    if (element) {
+        element.addEventListener("click", () => {
+            window.location.href = page;
+        });
+    }
+});
